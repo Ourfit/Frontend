65ff41240549f1f2b5e366ad8e87505cf13c0701
Jayjunyoung <no2jfamily@tukorea.ac.kr>
2025-01-02 17:19:44 +0900

    fix: 기존 코드 제거된 이슈 해결

---
+++ components/atoms/Typography/Styles.tsx
@@ -0,0 +1,179 @@
+import { css } from "styled-components";
+
+interface TypographyProps {
+  color?: string;
+}
+
+export const DefaultTypographyCss = css<TypographyProps>`
+  display: inline-flex;
+  align-items: center;
+  font-family: inherit;
+  font-style: normal;
+
+  color: ${({ color }) => color};
+`;
+
+const H1Bd = css`
+  ${DefaultTypographyCss};
+
+  font-size: 20px;
+  font-weight: 700;
+  line-height: 150%;
+  letter-spacing: -0.2px;
+`;
+
+const H1Sb = css`
+  ${DefaultTypographyCss};
+
+  font-size: 20px;
+  font-weight: 600;
+  line-height: 150%;
+  letter-spacing: -0.2px;
+`;
+
+const H2Bd = css`
+  ${DefaultTypographyCss};
+
+  font-size: 18px;
+  font-weight: 700;
+  line-height: 150%;
+  letter-spacing: -0.18px;
+`;
+
+const H2Sb = css`
+  ${DefaultTypographyCss};
+
+  font-size: 18px;
+  font-weight: 600;
+  line-height: 150%;
+  letter-spacing: -0.18px;
+`;
+
+const H3Bd = css`
+  ${DefaultTypographyCss};
+
+  font-size: 16px;
+  font-weight: 700;
+  line-height: 150%;
+  letter-spacing: -0.16px;
+`;
+
+const H3Sb = css`
+  ${DefaultTypographyCss};
+
+  font-size: 16px;
+  font-weight: 600;
+  line-height: 150%;
+  letter-spacing: -0.16px;
+`;
+
+const H3Md = css`
+  ${DefaultTypographyCss};
+
+  font-size: 16px;
+  font-weight: 500;
+  line-height: 150%;
+  letter-spacing: -0.16px;
+`;
+
+const H4Sb = css`
+  ${DefaultTypographyCss};
+
+  font-size: 14px;
+  font-weight: 600;
+  line-height: 150%;
+  letter-spacing: -0.14px;
+`;
+
+const H4Md = css`
+  ${DefaultTypographyCss};
+
+  font-size: 14px;
+  font-weight: 500;
+  line-height: 150%;
+  letter-spacing: -0.14px;
+`;
+
+const H4MdSpacing = css`
+  ${DefaultTypographyCss};
+
+  font-size: 14px;
+  font-weight: 500;
+  line-height: 180%;
+  letter-spacing: -0.14px;
+`;
+
+const H5Sb = css`
+  ${DefaultTypographyCss};
+
+  font-size: 13px;
+  font-weight: 600;
+  line-height: 150%;
+  letter-spacing: -0.13px;
+`;
+
+const H5Md = css`
+  ${DefaultTypographyCss};
+
+  font-size: 13px;
+  font-weight: 500;
+  line-height: 150%;
+  letter-spacing: -0.13px;
+`;
+
+const H6Sb = css`
+  ${DefaultTypographyCss};
+
+  font-size: 12px;
+  font-weight: 600;
+  line-height: 150%;
+  letter-spacing: -0.12px;
+`;
+
+const H6Md = css`
+  ${DefaultTypographyCss};
+
+  font-size: 12px;
+  font-weight: 500;
+  line-height: 150%;
+  letter-spacing: -0.12px;
+`;
+
+const H7Sb = css`
+  ${DefaultTypographyCss};
+
+  font-size: 10px;
+  font-weight: 600;
+  line-height: 150%;
+  letter-spacing: -0.1px;
+`;
+
+const H7Md = css`
+  ${DefaultTypographyCss};
+
+  font-size: 10px;
+  font-weight: 500;
+  line-height: 150%;
+  letter-spacing: -0.1px;
+`;
+
+const TypographyCss = {
+  H1Bd,
+  H1Sb,
+  H2Bd,
+  H2Sb,
+  H3Bd,
+  H3Sb,
+  H3Md,
+  H4Sb,
+  H4Md,
+  H4MdSpacing,
+  H5Sb,
+  H5Md,
+  H6Sb,
+  H6Md,
+  H7Sb,
+  H7Md,
+};
+
+export default TypographyCss;
---
+++ styles/theme.ts
@@ -0,0 +1,42 @@
+export const theme = {
+  colors: {
+    grayscale: {
+      50: "#f9f9fa",
+      100: "#f6f6f6",
+      200: "#e6e7ee",
+      300: "#dce0ea",
+      400: "#babfcc",
+      500: "#adb3ce",
+      600: "#8a92a3",
+      700: "#6c727f",
+      800: "#545862",
+      900: "#27282d",
+    },
+    blue: {
+      50: "#e6edff",
+      100: "#b0c8ff",
+      200: "#8aadff",
+      300: "#5487ff",
+      400: "#3370ff",
+      500: "#004dff",
+      600: "#1436e8",
+      700: "#0036b5",
+      800: "#002a8c",
+      900: "#00206b",
+    },
+    lime: {
+      500: "#cdff6f",
+    },
+    base: {
+      white: "#ffffff",
+    },
+    point: {
+      error: "#fa6767",
+      success: "#6795fa",
+    },
+    transparent: {
+      black50: "rgba(0, 0, 0, 0.5)",
+      black70: "rgba(0, 0, 0, 0.7)",
+    },
+  },
+};
