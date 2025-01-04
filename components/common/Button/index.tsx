dc75756b29834bdf185d3fac37036174b61c2576
권승민 <rnjstmdals6@naver.com>
2025-01-05 01:00:33 +0900

    feat: 버튼 컴포넌트 추가
    - 피그마에 표현된 것만 추가

---
+++ components/common/Button/index.tsx
@@ -0,0 +1,37 @@
+"use client";
+
+import React from "react";
+import * as S from "./Button.style";
+import { BUTTON_SIZES, BUTTON_VARIANTS } from "@/constants/Button";
+
+export interface ButtonProps {
+  children?: React.ReactNode;
+  disabled?: boolean;
+  size?: (typeof BUTTON_SIZES)[keyof typeof BUTTON_SIZES];
+  variant?: (typeof BUTTON_VARIANTS)[keyof typeof BUTTON_VARIANTS];
+  icon?: React.ReactNode;
+  onClick?: () => void;
+}
+
+const Button = ({
+  disabled = false,
+  size,
+  variant,
+  icon,
+  onClick,
+  children,
+}: ButtonProps) => {
+  return (
+    <S.Button
+      size={size}
+      variant={variant}
+      disabled={disabled}
+      onClick={onClick}
+    >
+      {icon && <S.IconWrapper>{icon}</S.IconWrapper>}
+      {children}
+    </S.Button>
+  );
+};
+
+export default Button;
