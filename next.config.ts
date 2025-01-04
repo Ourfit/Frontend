4fd6a8fe8cdcd79eb7851e6c2b709e2997a0b0cf
권승민 <rnjstmdals6@naver.com>
2025-01-05 00:57:35 +0900

    chore: svgr 모듈 설정
    - svg를 컴포넌트 형식으로 사용할 수 있음

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,23 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   reactStrictMode: true,
   compiler: {
-    styledComponents: true, // styled-components 지원 활성화
+    styledComponents: true,
+  },
+  webpack(config) {
+    // ✅ SVG 파일을 React 컴포넌트로 변환하는 로더 추가
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: [
+        {
+          loader: "@svgr/webpack",
+          options: {
+            icon: true,
+          },
+        },
+      ],
+    });
+
+    return config;
   },
 };
 
