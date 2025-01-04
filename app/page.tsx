13c57dada79db70a9d2ab934491c40483b1ed665
권승민 <rnjstmdals6@naver.com>
2025-01-05 02:01:18 +0900

    feat: 텍스트 버튼 예시 추가

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
 import Frame from "@/components/layout/Frame";
 import Button from "@/components/common/Button";
-import CheckIcon from "@/assets/images/check.svg";
+import EveningIcon from "@/assets/images/evening.svg";
+import PlusIcon from "@/assets/images/plus.svg";
 import { BUTTON_SIZES, BUTTON_VARIANTS } from "@/constants/Button";
+import TextButton from "@/components/common/TextButton";
 
 export default function Home() {
   return (
@@ -11,18 +13,8 @@ export default function Home() {
       <Button size={BUTTON_SIZES.LARGE} variant={BUTTON_VARIANTS.PRIMARY}>
         {"button L"}
       </Button>
-      <Button size={"m"} variant={"secondary"}>
-        {"button M"}
-      </Button>
-      <Button size={"xs"} variant={"outline"}>
-        {"button XS"}
-      </Button>
-      <Button size={"l"} variant={"primary"} disabled={true}>
-        {"button L"}
-      </Button>
-      <Button size={"m"} variant={"outline"} icon={<CheckIcon />}>
-        {"Icon "}
-      </Button>
+      <TextButton icon={<EveningIcon />}>{"text"}</TextButton>
+      <TextButton icon={<PlusIcon />}></TextButton>
     </Frame>
   );
 }
