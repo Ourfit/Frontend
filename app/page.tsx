e8c708e3b29b18e9cf667a092f0e741d9d660fcd
slppills <sunchaehoon2@naver.com>
2025-01-06 21:45:05 +0900

    feat: tab 컴포넌트 추가

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
+import Tab from "@/components/common/tab/Tab";
 import Frame from "@/components/layout/Frame";
 
 export default function Home() {
   return (
     <Frame>
+      <Tab tabs={["text", "text2", "text3"]} />
       <div style={{ textAlign: "center", height: "100px" }}>test</div>
       <div style={{ textAlign: "center", height: "100px" }}>test</div>
       <div style={{ textAlign: "center", height: "100px" }}>test</div>
