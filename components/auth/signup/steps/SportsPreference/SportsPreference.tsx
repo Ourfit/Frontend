071903bc426e89c1cf716237300657e7f9ad7e62
slppills <sunchaehoon2@naver.com>
2025-01-10 01:41:11 +0900

    style: SportsPreference 컴포넌트 스타일 분리

--- components/auth/signup/steps/SportsPreference/SportsPreference.tsx
+++ components/auth/signup/steps/SportsPreference/SportsPreference.tsx
@@ -2,56 +2,11 @@ import { StepProps } from "@/types/step";
 import { Typography } from "@/components/atoms/Typography";
 import { COLORS } from "@/constants/Theme";
 import Button from "@/components/common/Button";
+import * as S from "./SportsPreference.style";
 import { BUTTON_SIZES, BUTTON_VARIANTS } from "@/constants/Button";
 import { useState } from "react";
 import TextButton from "@/components/common/TextButton";
-import styled from "styled-components";
 import { SPORTS_LABEL, STEPS_LABEL } from "@/constants/Signup";
-
-const SportsPreferenceContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  gap: 32px;
-`;
-
-const SignupIntroContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  gap: 8px;
-`;
-
-const SignupIntroTitleWrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-  gap: 4px;
-`;
-
-const InfoContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: flex-start;
-  gap: 12px;
-`;
-
-const ButtonContainer = styled.div`
-  display: flex;
-  width: 100%;
-  justify-content: center;
-  align-items: center;
-`;
-
-const ButtonWrapper = styled.div`
-  box-sizing: border-box;
-  width: 65px;
-`;
-
-const TextButtonWrapper = styled.div`
-  display: flex;
-  gap: 10px;
-  width: 100%;
-  white-space: nowrap;
-  flex-wrap: wrap;
-`;
 
 const SportsPreference = ({ nextStep }: StepProps) => {
   const [selectedSports, setSelectedSports] = useState<string[]>([]);
@@ -75,20 +30,20 @@ const SportsPreference = ({ nextStep }: StepProps) => {
   };
 
   return (
-    <SportsPreferenceContainer>
-      <SignupIntroContainer>
-        <SignupIntroTitleWrapper>
+    <S.SportsPreferenceContainer>
+      <S.SignupIntroContainer>
+        <S.SignupIntroTitleWrapper>
           <Typography.H1Sb>
             선호하는 <span style={{ color: COLORS.BLUE_500 }}>운동</span>을
             선택해주세요!
           </Typography.H1Sb>
-        </SignupIntroTitleWrapper>
+        </S.SignupIntroTitleWrapper>
         <Typography.H4Md color={COLORS.GRAYSCALE_600}>
           최소 1개, 최대 3개까지 선택해주세요.
         </Typography.H4Md>
-      </SignupIntroContainer>
-      <InfoContainer>
-        <TextButtonWrapper>
+      </S.SignupIntroContainer>
+      <S.InfoContainer>
+        <S.TextButtonWrapper>
           {Object.values(SPORTS_LABEL).map((sport) => (
             <TextButton
               key={sport}
@@ -98,10 +53,10 @@ const SportsPreference = ({ nextStep }: StepProps) => {
               {sport}
             </TextButton>
           ))}
-        </TextButtonWrapper>
-      </InfoContainer>
-      <ButtonContainer>
-        <ButtonWrapper>
+        </S.TextButtonWrapper>
+      </S.InfoContainer>
+      <S.ButtonContainer>
+        <S.ButtonWrapper>
           <Button
             disabled={selectedSports.length === 0}
             size={BUTTON_SIZES.LARGE}
@@ -110,9 +65,9 @@ const SportsPreference = ({ nextStep }: StepProps) => {
           >
             다음
           </Button>
-        </ButtonWrapper>
-      </ButtonContainer>
-    </SportsPreferenceContainer>
+        </S.ButtonWrapper>
+      </S.ButtonContainer>
+    </S.SportsPreferenceContainer>
   );
 };
 
