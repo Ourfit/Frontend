03d408775aa776c29f39a30a60e0f50c937a4f64
slppills <sunchaehoon2@naver.com>
2025-01-10 01:23:39 +0900

    style: Nickname 컴포넌트 스타일 분리

---
+++ components/auth/signup/steps/Nickname/Nickname.style.ts
@@ -0,0 +1,31 @@
+import styled from "styled-components";
+
+export const NicknameContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 32px;
+`;
+
+export const SignupIntroContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 8px;
+`;
+
+export const SignupIntroTitleWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 4px;
+`;
+
+export const ButtonContainer = styled.div`
+  display: flex;
+  width: 100%;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const ButtonWrapper = styled.div`
+  box-sizing: border-box;
+  width: 65px;
+`;
--- components/auth/signup/steps/Nickname/Nickname.tsx
+++ components/auth/signup/steps/Nickname/Nickname.tsx
@@ -1,41 +1,11 @@
 import { Typography } from "@/components/atoms/Typography";
 import { COLORS } from "@/constants/Theme";
-import styled from "styled-components";
 import { StepProps } from "@/types/step";
+import * as S from "./Nickname.style";
 import Button from "@/components/common/Button";
 import { BUTTON_SIZES, BUTTON_VARIANTS } from "@/constants/Button";
 import React, { useState } from "react";
 import { STEPS_LABEL } from "@/constants/Signup";
-
-const NicknameContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  gap: 32px;
-`;
-
-const SignupIntroContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  gap: 8px;
-`;
-
-const SignupIntroTitleWrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-  gap: 4px;
-`;
-
-const ButtonContainer = styled.div`
-  display: flex;
-  width: 100%;
-  justify-content: center;
-  align-items: center;
-`;
-
-const ButtonWrapper = styled.div`
-  box-sizing: border-box;
-  width: 65px;
-`;
 
 const Nickname = ({ nextStep }: StepProps) => {
   const [nickname, setNickname] = useState("");
@@ -50,25 +20,25 @@ const Nickname = ({ nextStep }: StepProps) => {
     setNickname(e.target.value);
   };
   return (
-    <NicknameContainer>
-      <SignupIntroContainer>
-        <SignupIntroTitleWrapper>
+    <S.NicknameContainer>
+      <S.SignupIntroContainer>
+        <S.SignupIntroTitleWrapper>
           <Typography.H1Sb>반가워요!</Typography.H1Sb>
           <Typography.H1Sb>
             <span style={{ color: COLORS.BLUE_500 }}>닉네임</span>을
             설정해주세요
           </Typography.H1Sb>
-        </SignupIntroTitleWrapper>
+        </S.SignupIntroTitleWrapper>
         <Typography.H4Md color={COLORS.GRAYSCALE_600}>
           닉네임은 30일마다 변경할 수 있어요.
         </Typography.H4Md>
-      </SignupIntroContainer>
+      </S.SignupIntroContainer>
       <input
         placeholder={"한글만 입력 가능, 최대 12자"}
         onChange={handleInputChange}
       />
-      <ButtonContainer>
-        <ButtonWrapper>
+      <S.ButtonContainer>
+        <S.ButtonWrapper>
           <Button
             disabled={!nickname.trim()}
             size={BUTTON_SIZES.LARGE}
@@ -77,9 +47,9 @@ const Nickname = ({ nextStep }: StepProps) => {
           >
             다음
           </Button>
-        </ButtonWrapper>
-      </ButtonContainer>
-    </NicknameContainer>
+        </S.ButtonWrapper>
+      </S.ButtonContainer>
+    </S.NicknameContainer>
   );
 };
 
