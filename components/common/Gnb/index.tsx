49fa590a218c0b3ff8be820dba9ff2bd11fc96b6
Jayjunyoung <no2jfamily@tukorea.ac.kr>
2025-01-15 16:16:48 +0900

    design: 마이페이지, 오픈채팅 페이지에 사용될 layout 설정 및 오픈채팅 페이지 초안 적용

--- components/common/Gnb/index.tsx
+++ components/common/Gnb/index.tsx
@@ -1,43 +1,67 @@
 "use client";
 
-import * as S from "./Gnb.style";
+import BellIcon from "@/assets/images/bell.svg";
+import ChallengeIcon from "@/assets/images/challenge.svg";
 import HomeIcon from "@/assets/images/home.svg";
 import MateIcon from "@/assets/images/mate.svg";
-import ChallengeIcon from "@/assets/images/challenge.svg";
-import BellIcon from "@/assets/images/bell.svg";
 import SettingIcon from "@/assets/images/setting.svg";
 import { GNB_LABELS, GNB_TAB_IDS } from "@/constants/Gnb";
+import { usePathname, useRouter } from "next/navigation";
 import { useState } from "react";
+import * as S from "./Gnb.style";
 
 const GNB_TABS = [
-  { id: GNB_TAB_IDS.HOME, label: GNB_LABELS.HOME, icon: <HomeIcon /> },
-  { id: GNB_TAB_IDS.MATE, label: GNB_LABELS.MATE, icon: <MateIcon /> },
+  {
+    id: GNB_TAB_IDS.HOME,
+    label: GNB_LABELS.HOME,
+    path: "/",
+    icon: <HomeIcon />,
+  },
+  {
+    id: GNB_TAB_IDS.MATE,
+    label: GNB_LABELS.MATE,
+    path: "/mate",
+    icon: <MateIcon />,
+  },
   {
     id: GNB_TAB_IDS.CHALLENGE,
     label: GNB_LABELS.CHALLENGE,
+    path: "/challenge",
     icon: <ChallengeIcon />,
   },
   {
     id: GNB_TAB_IDS.NOTIFICATIONS,
     label: GNB_LABELS.NOTIFICATIONS,
+    path: "/notifications",
     icon: <BellIcon />,
   },
   {
     id: GNB_TAB_IDS.SETTINGS,
     label: GNB_LABELS.SETTINGS,
+    path: "/mypage",
     icon: <SettingIcon />,
   },
 ];
 
 const Gnb = () => {
   const [activeTabId, setActiveTabId] = useState<number>(GNB_TAB_IDS.HOME);
+
+  const router = useRouter();
+
+  const pathname = usePathname();
+
+  const handleTabClick = (path: string) => {
+    if (pathname !== path) {
+      router.push(path);
+    }
+  };
   return (
     <S.GnbContainer>
       {GNB_TABS.map((tab) => (
         <S.GnbTabWrapper
           key={tab.id}
-          $isActive={tab.id === activeTabId}
-          onClick={() => setActiveTabId(tab.id)}
+          $isActive={tab.path === pathname}
+          onClick={() => handleTabClick(tab.path)}
         >
           {tab.icon}
           {tab.label}
--- components/layout/Frame.tsx
+++ components/layout/Frame.tsx
@@ -2,11 +2,6 @@
 
 import Gnb from "@/components/common/Gnb";
 import Header from "@/components/common/Header/Header";
-import Input from "@/components/common/Input/Input";
-import Toast from "@/components/common/Toast/Toast";
-import Tooltip from "@/components/common/Tooltip/Tooltip";
-import { TOAST_MESSAGES, TOAST_STATUSES } from "@/constants/Toast";
-import { TOOLTIP_POSITIONS } from "@/constants/TooltipPositions";
 import React, { useEffect, useState } from "react";
 import * as S from "./Frame.style";
 
@@ -29,12 +24,7 @@ export default function Frame({ children }: Readonly<FrameProps>) {
 
   return (
     <S.FrameContainer>
-      <Tooltip text="supporting text" position={TOOLTIP_POSITIONS.TOP_LEFT} />
       <Header />
-      {isToastVisible && (
-        <Toast message={TOAST_MESSAGES.ERROR} status={TOAST_STATUSES.ERROR} />
-      )}
-      <Input />
       <S.Content>{children}</S.Content>
       <Gnb />
     </S.FrameContainer>
