a0315b2253cd14ee709f431ca976d5ca8297b163
hzxng <hazung27@gmail.com>
2025-01-27 17:09:17 +0900

    design: step indicator 크기 수정 및 padding 설정

--- components/common/StepIndicator/StepIndicator.style.ts
+++ components/common/StepIndicator/StepIndicator.style.ts
@@ -1,15 +1,17 @@
 import { COLORS } from "@/constants/Theme";
-import EllipseIcon from "@/assets/images/ellipse.svg";
 import styled from "styled-components";
 
 export const StepContainer = styled.div`
   display: flex;
   gap: 4px;
+  align-items: center;
 `;
 
-export const Ellipse = styled(EllipseIcon)<{ $isActive: boolean }>`
-  & > circle {
-    fill: ${({ $isActive }) =>
+export const Ellipse = styled.div<{ $isActive: boolean }>`
+  & > svg {
+    width: 6px;
+    height: 6px;
+    color: ${({ $isActive }) =>
       $isActive ? COLORS.BLUE_500 : COLORS.GRAYSCALE_200} !important;
   }
 `;
