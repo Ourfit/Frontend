42ec5ae80d1299ea4f57673a537902cdb4d6d2d5
Jayjunyoung <no2jfamily@tukorea.ac.kr>
2025-02-11 13:26:00 +0900

    feat: 검색 쪽 임시 디자인 및 검색 기능 구현

--- components/common/Header/Header.tsx
+++ components/common/Header/Header.tsx
@@ -3,11 +3,12 @@
 import ChevronLeft from "@/assets/images/chevron-left.svg";
 import Location from "@/assets/images/location.svg";
 import OurfitLogo from "@/assets/images/ourfit-logo.svg";
+import SearchIcon from "@/assets/images/search.svg";
 import { Typography } from "@/components/atoms/Typography";
-import { COLORS } from "@/constants/Theme";
-import { usePathname } from "next/navigation";
-import * as S from "./Header.style";
 import { GNB_LABELS } from "@/constants/Gnb";
+import { COLORS } from "@/constants/Theme";
+import { usePathname, useRouter } from "next/navigation";
+import * as S from "./Header.style";
 
 interface HeaderProps {
   isEditingProfile?: boolean;
@@ -34,6 +35,7 @@ export default function Header({
     "/mate/facility": "운동 시설",
     "/mate/time": "운동 시간",
     "/mate/mateprofile": "프로필",
+    "/mate/search": "검색",
   };
 
   const isHome = pathname === "/";
@@ -45,6 +47,8 @@ export default function Header({
   const isGnbTab = Object.values(GNB_LABELS).includes(
     pageNames[pathname] as GnbLabel,
   );
+
+  const router = useRouter();
 
   return (
     <>
@@ -79,10 +83,19 @@ export default function Header({
           </Typography.H2Sb>
         </S.HeaderContainer>
       ) : isGnbTab || isChallenge ? (
-        <S.HeaderContainer $paddingLeft="20px" $paddingRight="0">
+        <S.HeaderContainer
+          $paddingLeft="20px"
+          $paddingRight="20px"
+          $justifyContent="space-between"
+        >
           <Typography.H1Sb color={COLORS.GRAYSCALE_900}>
             {isChallenge ? "챌린지" : pageNames[pathname]}
           </Typography.H1Sb>
+          <SearchIcon
+            style={{ width: "24px", height: "24px" }}
+            fill="current"
+            onClick={() => router.push("/mate/search")}
+          />
         </S.HeaderContainer>
       ) : (
         <S.HeaderContainer
