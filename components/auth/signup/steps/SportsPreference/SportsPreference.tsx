15cb98bae291131c83322c7ae4f5ed5acfbe56ec
Oh, Sukyeong <osukyeong0109@gmail.com>
2025-02-19 17:27:49 +0900

    chore: 불필요한 코드 정리

--- components/auth/signup/steps/SportsPreference/SportsPreference.tsx
+++ components/auth/signup/steps/SportsPreference/SportsPreference.tsx
@@ -3,9 +3,9 @@ import { Typography } from "@/components/atoms/Typography";
 import { COLORS } from "@/constants/Theme";
 import Button from "@/components/common/Button";
 import * as S from "./SportsPreference.style";
+import * as CS from "@/components/common/TextButton/TextButton.style";
 import { BUTTON_SIZES, BUTTON_VARIANTS } from "@/constants/Button";
 import { SPORTS_LABEL, STEPS_LABEL } from "@/constants/Signup";
-import TextButton from "@/components/common/TextButton";
 import { usePathname, useRouter } from "next/navigation"; 
 import { useState } from "react";
 
@@ -57,13 +57,13 @@ const SportsPreference = ({ nextStep }: StepProps) => {
         <S.InfoContainer>
           <S.TextButtonWrapper>
             {Object.values(SPORTS_LABEL).map((sport) => (
-              <TextButton
+              <CS.TextButton
                 key={sport}
-                isActive={selectedSports.includes(sport)}
+                $isActive={selectedSports.includes(sport)}
                 onClick={() => handleSportClick(sport)}
               >
                 {sport}
-              </TextButton>
+              </CS.TextButton>
             ))}
           </S.TextButtonWrapper>
         </S.InfoContainer>
--- components/common/TextButton/TextButton.style.ts
+++ components/common/TextButton/TextButton.style.ts
@@ -7,51 +7,46 @@ interface TextButtonStyleProps {
   $hasIcon?: boolean;
 }
 
-export const getTextButtonStyle = ({ $isActive, $hasIcon }: TextButtonStyleProps) => {
-  const COMMON_CSS = css`
-    display: flex;
-    align-items: center;
-    gap: 4px;
-    border-radius: 12px;
-    cursor: pointer;
+const COMMON_CSS = css`
+  display: flex;
+  align-items: center;
+  gap: 4px;
+  border-radius: 12px;
+  cursor: pointer;
+  ${TypographyCss.H4Md};
 
-    border: 1px solid ${$isActive ? COLORS.BLUE_500 : COLORS.GRAYSCALE_200};
-    color: ${$isActive ? COLORS.BLUE_500 : COLORS.GRAYSCALE_600};
-    background: ${$isActive ? COLORS.BLUE_50 : COLORS.BASE_WHITE};
-    ${TypographyCss.H4Md};
-
-    &:hover {
-      color: ${COLORS.BLUE_500};
-      border: 1px solid ${COLORS.BLUE_500};
-      background: ${COLORS.BLUE_200};
-    }
-
-    @media (max-width: 360px) {
-      padding: ${$hasIcon ? "12px 12px" : "12px 16px"};
-    }
-
-    @media (max-width: 375px) and (min-width: 361px) {
-      padding: ${$hasIcon ? "12px 12px 12px 16px" : "12px 20px"};
-    }
-
-    @media (max-width: 390px) and (min-width: 375px) {
-      padding: ${$hasIcon ? "12px 12px 12px 16px" : "12px 20px"};
-    }
-
-    @media (max-width: 412px) and (min-width: 390px) {
-      padding: ${$hasIcon ? "12px 16px 12px 16px" : "12px 20px"};
-    }
-
-    @media (min-width: 412px) {
-      padding: ${$hasIcon ? "12px 24px 12px 18px" : "12px 20px"};
-    }
-  `;
-
-  return css`
-    ${COMMON_CSS};
-  `;
-};
+  &:hover {
+    color: ${COLORS.BLUE_500};
+    border: 1px solid ${COLORS.BLUE_500};
+    background: ${COLORS.BLUE_200};
+  }
+`;
 
 export const TextButton = styled.button<TextButtonStyleProps>`
-  ${({ $isActive, $hasIcon }) => getTextButtonStyle({ $isActive, $hasIcon })}
+  ${COMMON_CSS};
+  padding: ${({ $hasIcon }) => ($hasIcon ? "12px 12px" : "12px 16px")};
+
+  border: 1px solid ${({ $isActive }) => ($isActive ? COLORS.BLUE_500 : COLORS.GRAYSCALE_200)};
+  color: ${({ $isActive }) => ($isActive ? COLORS.BLUE_500 : COLORS.GRAYSCALE_600)};
+  background: ${({ $isActive }) => ($isActive ? COLORS.BLUE_50 : COLORS.BASE_WHITE)};
+
+  @media (max-width: 360px) {
+    padding: ${({ $hasIcon }) => ($hasIcon ? "12px 12px" : "12px 16px")};
+  }
+
+  @media (max-width: 375px) and (min-width: 361px) {
+    padding: ${({ $hasIcon }) => ($hasIcon ? "12px 12px 12px 16px" : "12px 20px")};
+  }
+
+  @media (max-width: 390px) and (min-width: 375px) {
+    padding: ${({ $hasIcon }) => ($hasIcon ? "12px 12px 12px 16px" : "12px 20px")};
+  }
+
+  @media (max-width: 412px) and (min-width: 390px) {
+    padding: ${({ $hasIcon }) => ($hasIcon ? "12px 16px 12px 16px" : "12px 20px")};
+  }
+
+  @media (min-width: 412px) {
+    padding: ${({ $hasIcon }) => ($hasIcon ? "12px 24px 12px 18px" : "12px 20px")};
+  }
 `;
