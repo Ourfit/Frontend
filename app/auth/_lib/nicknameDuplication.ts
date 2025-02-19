b31ce534c668f183be26b1fb72df70f7d10c6504
hzxng <hazung27@gmail.com>
2025-02-19 12:09:19 +0900

    refactor: 닉네임 중복 응답 분기처리 수정

--- app/auth/_lib/nicknameDuplication.ts
+++ app/auth/_lib/nicknameDuplication.ts
@@ -4,14 +4,15 @@ export async function nicknameDuplication(nickname: string) {
       `${process.env.NEXT_PUBLIC_BASE_URL}/v1/users/check-availability?field=nickname&value=${nickname}`,
     );
 
-    if (response.status === 204) {
-      return { available: true, message: "" };
-    } else if (response.status === 409) {
-      return { available: false, message: "중복되는 닉네임이에요" };
-    } else if (response.status === 400) {
-      return { available: false, message: "잘못된 형식이에요" };
-    } else {
-      return { available: false, message: "오류 발생" };
+    switch (response.status) {
+      case 204:
+        return { available: true, message: "" };
+      case 409:
+        return { available: false, message: "중복되는 닉네임이에요" };
+      case 400:
+        return { available: false, message: "잘못된 형식이에요" };
+      default:
+        return { available: false, message: "오류 발생" };
     }
   } catch (err) {
     console.log(err);
