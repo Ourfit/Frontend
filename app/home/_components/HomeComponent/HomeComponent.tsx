5f5096c9c8a6dcee5d38be7736c89e6190cc058d
hzxng <hazung27@gmail.com>
2025-02-25 17:39:43 +0900

    fix: 절대 경로 변경

--- app/home/_components/HomeComponent/HomeComponent.tsx
+++ app/home/_components/HomeComponent/HomeComponent.tsx
@@ -13,7 +13,7 @@ import { useRouter } from "next/navigation";
 import { useEffect } from "react";
 import { useOAuthIdStore } from "@/stores/oAuthIdStore";
 import { useQuery } from "@tanstack/react-query";
-import getUserMe from "../../_lib/getUserMe";
+import getUserMe from "@/app/home/_lib/getUserMe";
 
 const PageContainer = styled.div`
   overflow-y: scroll;
