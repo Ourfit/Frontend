8c25ad79b0f8badd39d2e012d2e4db18c397bd9e
Jayjunyoung <no2jfamily@tukorea.ac.kr>
2025-03-04 11:51:59 +0900

    feat: staleTime 수정

--- hooks/queries/useWorkoutTypes.ts
+++ hooks/queries/useWorkoutTypes.ts
@@ -5,6 +5,6 @@ export function useWorkoutTypes() {
   return useQuery({
     queryKey: ["workoutTypes"],
     queryFn: fetchWorkoutTypes,
-    staleTime: 60 * 1000, // 1분 동안 데이터 캐싱
+    staleTime: 60 * 1000 * 20,
   });
 }
