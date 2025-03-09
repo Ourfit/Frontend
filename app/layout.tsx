911266e3a8d202c77bc3540e1310e77b41a5bbf6
hzxng <hazung27@gmail.com>
2025-03-09 14:09:04 +0900

    add: 모바일 favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -39,6 +39,11 @@ export const metadata: Metadata = {
     ],
     type: "website",
   },
+  icons: {
+    icon: "/favicon.ico",
+    shortcut: "/favicon.ico",
+    apple: "/apple-touch-icon.png",
+  },
 };
 
 export default function RootLayout({
